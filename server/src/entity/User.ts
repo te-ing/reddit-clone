7b9ee531b3dafcca8c61ce491ec517b4d31690e3
te-ing <hi2177@naver.com>
2022-09-12 15:33:05 +0900

    Entity 생성: ~ 서버실행해서 나오는 에러 해결

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,7 +1,38 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { IsEmail, Length, ValidationTypes } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, Column, Index, OneToMany, BeforeInsert } from "typeorm"
+import bcrypt from 'bcryptjs'
+import Post from "./Post";
+import Vote from "./Vote";
+import BaseEntity from "./Entity";
+@Entity("users")
+export class User extends BaseEntity{
 
-@Entity()
-export class User {
+    @Index()
+    @IsEmail(undefined, {message: "이메일 주소가 잘못되었습니다."})
+    @Length(1, 255, {message: "이메일 형식이 잘못되었습니다."})
+    @Column({ unique: true })
+    emial: string;
+    
+    @Index()
+    @Length(3, 255, {message: "이메일 형식이 잘못되었습니다."})
+    @Column({unique: true})
+    username: string;
+
+    @Column()
+    @Length(6, 255, { message: '비밀번호는 6자리 이상이어야 합니다.' })
+    @Column()
+    password: string;
+
+    @OneToMany(() => Post, (post) => post.user)
+    posts: Post[]
+
+    @OneToMany(() => Vote, (vote) => vote.user)
+    votes: Vote[]
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 6)
+    }
 
     @PrimaryGeneratedColumn()
     id: number
